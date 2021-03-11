e1d6fe1a44b329e55c896a9e45c8cc4c8d91d5ad
srezasm <srezas.2017@gmail.com>
2021-03-11 18:49:13 +0330

    ErrorHandling Completed

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Middleware.ErrorHandling.Controllers
+{
+   [Route("Error")]
+   public class ErrorController : Controller
+   {
+      public IActionResult Error404()
+      {
+         return View();
+      }
+
+      [Route("404")]
+      public IActionResult Error(string statusCode)
+      {
+         return Content($"Error Code is: {statusCode}");
+      }
+   }
+}
